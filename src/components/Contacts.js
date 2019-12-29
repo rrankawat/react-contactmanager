7895ae83b09d4ac9ebfcceb374a7950db032bdab
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2019-12-30 00:28:19 +0530

    context api, contact delete

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,50 +1,24 @@
 import React, { Component, Fragment } from 'react';
 import Contact from './Contact';
 
+import { Consumer } from '../context';
+
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '444-444-4444'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '333-333-3333'
-      }
-    ]
-  };
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-
-    const newContacts = contacts.filter(contact => contact.id != id);
-    this.setState({ contacts: newContacts });
-  };
-
   render() {
-    const { contacts } = this.state;
-
     return (
-      <Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+
+          return (
+            <Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
